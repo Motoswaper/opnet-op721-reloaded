9d8a6d3d84033d5a6b3a182138c7eb49051ef455
Motoswaper <cumanes.com@gmail.com>
2026-04-16 01:46:46 -0400

    Delete contracts/interfaces/AddressUtils.ts

--- contracts/interfaces/AddressUtils.ts
+++
@@ -1,7 +0,0 @@
-// contracts/utils/AddressUtils.ts
-
-import { Address } from '../interfaces/IOP721'
-
-export function isZeroAddress(addr: Address): boolean {
-  return addr === '0x0000000000000000000000000000000000000000'
-}
